847eddf7508e14266b10862855497e3e202c4b01
Luis Gonzalez <lmgl887@gmail.com>
2022-03-05 03:17:22 -0600

    redesign practice2 UI

--- practica2/app.js
+++ practica2/app.js
@@ -8,9 +8,9 @@ let data = browser.getElementsByTagName("li");
 function getData(){
   
   browser.innerHTML = `
-  <div class="card" style="width: 40rem;">
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item text-start"><span class="fw-bold">Browser name: </span>${navigator.userAgent} </li>
+  <div class="card" style="width: 50rem;">
+  <ul class="list-group list-group-flush bg-dark">
+    <li class="list-group-item text-start "><span class="fw-bold">Browser name: </span>${navigator.userAgent} </li>
     <li class="list-group-item text-start"><span class="fw-bold">Browser Version: </span>${navigator.appVersion} </li>
     <li class="list-group-item text-start"><span class="fw-bold">Network status: </span>${navigator.onLine} </li>
     <li class="list-group-item text-start"><span class="fw-bold">Browser Vendor: </span>${navigator.vendor} </li>
