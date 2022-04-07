cb734fcbafa5a7e7b3b43a2fb5b4e48b48745988
Luis Gonzalez <lmgl887@gmail.com>
2022-04-07 18:25:41 -0500

    pr9 dark map

--- fetch/practica9/app.js
+++ fetch/practica9/app.js
@@ -1,16 +1,29 @@
 
 function initMap(){
+
+
+  let styleMap = new google.maps.StyledMapType([
+    { elementType: 'geometry', stylers: [{ color: '#242f3e'}]},
+    { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+    { elementType: 'labels.text.fill', stylers: [{ color: '#7f97b8' }] }
+  ], {name: 'Dark Mode'});
+
+
   let properties = {
     center: {
-      lat:21.152639,
-      lng: -101.711598
+      lat: 35.718008201206054,
+      lng: 139.78970612599443
     },
-    zoom: 14
+    zoom: 18,
+    mapTypeControlOptions: ['roadmap', 'satellite', 'hybrid', 'terrain', 'style_map']
   };
 
   let map = document.getElementById('map');
 
   let mapGoogle = new google.maps.Map(map, properties);
 
-  
+  map.mapTypes.set('style_map', styleMap);
+  map.setMapTypeId('style_map')
+
+
 }
