91c6e87f00151005a515baa551216260b0bfaa3c
Luis Gonzalez <lmgl887@gmail.com>
2022-04-07 17:57:35 -0500

    pr8 add map

---
+++ fetch/practica8/app.js
@@ -0,0 +1,31 @@
+
+
+
+let coordinates = {
+  lat: 29.099358,
+  lng: -110.956896
+};
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const language = urlParams.get("language");
+
+let script = document.createElement('script');
+
+script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCMrQgga-C5zCuZLTVk2MPVzX7naqKZXZU&callback=initMap&language=${language}`;
+
+document.head.appendChild(script);
+
+const initMap = () => {
+  let map = new google.maps.Map(document.getElementById("map"),
+  {
+    center: coordinates,
+    zoom: 3
+  }
+ );
+}
+//initMap()
+
+
+
+
