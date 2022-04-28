3ceb312a213cfb1a0bf501c624bc28a62d51b1a6
Luis Gonzalez <lmgl887@gmail.com>
2022-04-28 18:05:57 -0500

    pr11, map1-2-3-4

---
+++ fetch/practica11/app.js
@@ -0,0 +1,40 @@
+function initMap(){
+  let coordinates = {
+    lat: 21.152639,
+    lng: -101.711598
+  }
+
+  let propsMap1 = {
+    center: coordinates,
+    zoom: 17,
+    disableDefaultUI: true
+  }
+  map1 = new google.maps.Map(document.getElementById('map1'), propsMap1);
+
+  let propsMap2 = {
+    center: coordinates,
+    zoom: 17,
+    disableDefaultUI: true
+  }
+  map2 = new google.maps.Map(document.getElementById('map2'), propsMap2);
+
+  let propsMap3 = {
+    center: coordinates,
+    zoom: 17,
+    zoomControl: false,
+    scaleControl: false
+  }
+  map3 = new google.maps.Map(document.getElementById('map3'), propsMap3);
+
+  let propsMap4 = {
+    center: coordinates,
+    zoom: 17,
+    mapTypeControl: true,
+    mapTypeControlOption: {
+      style: google.maps.mapTypeControlStyle.DROPDOWN_MENU,
+      mapTypeIds: ['roadmap', 'satellite', 'terrain']
+    }
+
+  }
+  map4 = new google.maps.Map(document.getElementById('map4'), propsMap4);
+}
